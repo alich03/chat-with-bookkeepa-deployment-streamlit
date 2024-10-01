99255c0ec7ca4601f339ae251bb166ba48300966
Ali Hasnain <ali_hasnain@digimarkdevelopers.com>
2024-10-01 18:01:09 +0500

    prompt and pinecone updated

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ from dotenv import load_dotenv
 load_dotenv()
 from time import sleep
 
-# os.environ['PINECONE_API_KEY'] = os.getenv("PINECONE_API_KEY")
-# os.environ['openai_api_key'] = os.getenv("OPEN_AI_KEY")
-
 
 def chatbot_rag():
     st.title("Chat with PDFs BOOKKEEPA")
@@ -30,13 +27,20 @@ def chatbot_rag():
             if message["content_urls"]:
                 image_links = message["content_urls"]
                 if isinstance(image_links, list):
-                    # Display multiple images side by side using columns
-                    cols = st.columns(len(image_links))
-                    for col, image_link in zip(cols, image_links):
-                        col.image(image_link, caption='Image from manual', width=100, use_column_width=False)
+                    images_per_row = 5
+# Iterate over the image links in chunks of 5
+                    for i in range(0, len(image_links), images_per_row):
+                        # Create a row of 5 columns (or fewer for the last row)
+                        cols = st.columns(min(images_per_row, len(image_links) - i))
+                        # Render each image in its corresponding column
+                        for j, img_url in enumerate(image_links[i:i + images_per_row]):
+                            with cols[j]:
+                                clean_url = img_url.replace(" ", "")
+                                st.image(clean_url, width=100, use_column_width=False)  # Adjust image width
                 else:
                     # Display single image
-                    st.image(image_links, caption='Image from manual', width=100, use_column_width=False)
+                    clean_url = image_links.replace(" ", "")
+                    st.image(clean_url, width=100, use_column_width=False)
 
     # Input from user
     if prompt := st.chat_input("How can I help you for Bookkeepa ..."):
@@ -58,7 +62,7 @@ def chatbot_rag():
 
             for i in range(len(structured_answer['response'])):
                 progress_text.markdown(structured_answer['response'][:i + 1])
-                sleep(0.0001)
+                sleep(0.00003)
 
             if image_links:
                 if isinstance(image_links, list):
@@ -71,9 +75,11 @@ def chatbot_rag():
                         # Render each image in its corresponding column
                         for j, img_url in enumerate(image_links[i:i + images_per_row]):
                             with cols[j]:
-                                st.image(img_url, width=100, use_column_width=False)  # Adjust image width
+                                clean_url = img_url.replace(" ", "")
+                                st.image(clean_url, width=100, use_column_width=False)  # Adjust image width
                 else:
-                    st.image(image_links, width=100, use_column_width=False)
+                    clean_url = image_links.replace(" ", "")
+                    st.image(clean_url, width=100, use_column_width=False)
 
         # Store bot response in session state
         st.session_state.messages.append({
@@ -91,24 +97,25 @@ def chatbot_rag():
 def get_response_from_api(user_input):
     llm = ChatOpenAI(
     api_key=os.environ.get('OPEN_AI_KEY'),
-    model='gpt-4',  # or 'gpt-3.5-turbo'
-    temperature=0.4,
-    max_tokens=4096  # Adjust token limit as per the model
+    model='chatgpt-4o-latest',  # or 'gpt-3.5-turbo'
+    temperature=0.3,
+    max_tokens=5096  # Adjust token limit as per the model
         )
     embed_model = OpenAIEmbeddings(api_key=os.getenv("OPEN_AI_KEY"))
 
-    index_name = "rag-bookepa" 
+    index_name = "rag1" 
 
     pcv= PineconeVectorStore(index_name=index_name,embedding=embed_model,pinecone_api_key=os.getenv("PINECONE_API_KEY"))
 
     # vector_store = PineconeVectorStore.from_existing_index(index_name=index_name,embedding=embed_model)
-    retriever = pcv.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+    retriever = pcv.as_retriever(search_type="similarity", search_kwargs={"k": 5})
 
     retrieval_qa_chat_prompt = PromptTemplate(
     input_variables=["context", "input"],
-    template='''
+    template=
+            '''
             %INSTRUCTIONS:
-            You are Doctor Bookkeepa, a chatbot designed to assist users with our app. Your main task is to retrieve relevant context based on the user’s prompt and provide appropriate instructions. If the context contains image urls, please extract and include those as well and please dont cut them off, give the entire urls. 
+            You are Doctor Bookkeepa, a chatbot designed to assist users based on a given manual or FAQ document. Your task is to guide user with relevant context from the document based on the user's prompt and provide accurate instructions without adding or omitting steps. If the context contains image urls, please extract and include those as well. Please ensure that we need a proper list of URLs. Ensure that your responses are concise, precise, and strictly aligned with the manual's content.
 
             Your response must strictly adhere to the following JSON format:
 
@@ -121,6 +128,7 @@ def get_response_from_api(user_input):
             %QUESTION: {input}
 
             ''')
+
 
 
     combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
